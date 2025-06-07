a51712d30bc6b6d643049783e8d9d9c6338bbc99
José Vergara <josea.vergara2005@gmail.com>
2025-06-07 01:13:35 -0600

    Init

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<script setup>
+import IconSettings from '@/components/icons/IconSettings.vue';
+import IconMorlan from './icons/IconMorlan.vue';
+</script>
+
+<template>
+  <header class="flex justify-between items-center px-6 py-1">
+    <div>
+      <IconMorlan class="w-16"/>
+    </div>
+    <div class="flex flex-col items-center">
+      <span class="text-2xl">Morlan's</span>
+      <span>Homogeneous</span>
+      <span>Destroyer</span>
+    </div>
+    <div>
+      <IconSettings class="fill-black" />
+    </div>
+  </header>
+</template>
---
+++ src/components/icons/IconMorlan.vue
@@ -0,0 +1,28 @@
+<template>
+<svg
+   width="120"
+   height="120"
+   viewBox="0 0 120 120"
+   version="1.1"
+   id="svg1"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <defs
+     id="defs1" />
+  <g
+     id="layer1">
+    <path
+       style="fill:#4682b4;fill-opacity:1;stroke:#26619c;stroke-width:8;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:1.1;stroke-dasharray:none;stroke-opacity:1;paint-order:stroke fill markers"
+       d="M 15.000075,105 V 15 h 15 C 30.000075,15 29.89547,44.860628 60,45 90.10453,45.139373 90.000075,15 90.000075,15 H 105.00008 L 105,105 H 90.000075 V 45 C 90.000075,45 81.045296,59.442509 60,60 38.954704,60.557492 30.000075,45 30.000075,45 v 60 z"
+       id="path1" />
+    <ellipse
+       style="fill:#26619c;fill-opacity:1;stroke:none;stroke-width:4.76478;stroke-linecap:butt;stroke-linejoin:miter;stroke-miterlimit:1.5;stroke-dasharray:none;stroke-opacity:1;paint-order:stroke fill markers"
+       id="path2"
+       cx="60"
+       cy="22.499805"
+       rx="12.617612"
+       ry="12.617414" />
+  </g>
+</svg>
+
+</template>
