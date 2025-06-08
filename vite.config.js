f17df84e84883bcf477f88a833344dfe9c0967af
P0nchyz <74800258+P0nchyz@users.noreply.github.com>
2025-06-08 00:18:04 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Morlan-s-Homogeneous-Destroyer/',
   plugins: [
     vue(),
     vueDevTools(),
